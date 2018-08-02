acb3baf2737c10f98b9ca7f04477dc663499623d
jgkr95 <jgkr95@msitprogram.net>
2018-08-02 17:34:17 +0530

    " -> 0 of 4 passed. pylint: 10/10 "

---
+++ Practice/M4/p1/vowels_counter.py
@@ -0,0 +1,15 @@
+''' Assume s is a string of lower case characters. '''
+#that counts up the number of vowels contained in the string s
+#Number of vowels: 5
+
+def main():
+    '''This is the main method'''
+    st_vo = input()
+    vowels = 0
+    for i in st_vo:
+        if i in ("a", "e", "i", "o", "u"):
+            vowels = vowels+1
+    print(vowels)
+
+if __name__ == "__main__":
+    main()
