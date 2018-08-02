f8a793465b5481b717b40480050fad94d37408ad
jgkr95 <jgkr95@msitprogram.net>
2018-08-02 14:58:57 +0530

    " -> 1 of 1 passed. pylint: 10/10 "

---
+++ Practice/M4/sample-assignment/sol.py
@@ -0,0 +1,14 @@
+'''Exercise: Assignment-1'''
+# Write a Python function, factorial(n), that takes in one number and
+# returns adiition of digits in two digit number.
+
+# This function takes in one number and returns one number.
+
+def main():
+    ''' This is the main method'''
+    data = input()
+    # addition of two numbers
+    print(int(data[0])+int(data[1]))
+
+if __name__ == "__main__":
+    main()
---
+++ Practice/for trace.py
@@ -0,0 +1,3 @@
+n=2
+for num in range(0,10,2):
+	print(num/n)
