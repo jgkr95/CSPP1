3f1d19b43ba34c9cc794a5ee82b85cee13298153
jgkr95 <jgkr95@msitprogram.net>
2018-08-03 15:29:35 +0530

    " -> 3 of 4 passed. pylint: 10/10 "

--- Practice/M5/p1/perfect_cube.py
+++ Practice/M5/p1/perfect_cube.py
@@ -2,13 +2,12 @@
  cube or not
 using guess and check algorithm'''
 def main():
-    ''' input is captured in s'''
+    '''input is captured in s'''
     s_r = int(input())
-    n_n = 0
     for i in range(s_r):
         if i**3 == s_r:
             print(str(s_r)+" is a perfect cube")
-            break    
+            break
     else:
         print(str(s_r)+" is not a perfect cube")
 if __name__ == "__main__":
