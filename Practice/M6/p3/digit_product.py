d5e91477391235572355ade1de31380afdbdfdb1
jgkr95 <jgkr95@msitprogram.net>
2018-08-04 11:25:07 +0530

    " -> 7 of 7 passed. pylint: 8/10 "

--- Practice/M6/p3/digit_product.py
+++ Practice/M6/p3/digit_product.py
@@ -7,26 +7,26 @@ example:
 def main():
     '''Read any number from the input, store it in variable int_input.'''
     int_input = int(input())
-    m_in = 1
-    if int_input == 0:
-        print(str(int_input))
-    else:    
-        if int_input < 0:
-            int_in = -1 * int_input
-            while int_in > 0:
-                rem_i = int_in % 10
-                m_in = m_in * rem_i
-                int_in = int_in // 10
-            m_in = m_in * -1
-            print(str(m_in))
-            break
-        else:
-            while int_input > 0:
-                rem_i = int_input % 10
-                m_in = m_in * rem_i
-                int_input = int_input // 10
-                print(str(m_in))
-                break
+    m_in = 1      
+    if int_input < 0:
+        int_in = -1 * int_input
+        while int_in > 0:
+            rem_i = int_in % 10
+            m_in = m_in * rem_i
+            int_in = int_in // 10
+        m_in = m_in * -1
+        print(str(m_in))
+            
+    elif int_input > 0:
+        i_i = 0
+        while int_input > 0:
+            rem_i = int_input % 10
+            m_in = m_in * rem_i
+            int_input = int_input // 10
+        print(str(m_in))
+    else:
+        print("0")
+                
 
 if __name__ == "__main__":
     main()
