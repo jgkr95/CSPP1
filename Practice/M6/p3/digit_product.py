bb4e28bca847b236cde95d22e917ffdb0cecfda9
jgkr95 <jgkr95@msitprogram.net>
2018-08-04 11:10:59 +0530

    " -> 3 of 7 passed. pylint: 9/10 "

--- Practice/M6/p3/digit_product.py
+++ Practice/M6/p3/digit_product.py
@@ -10,20 +10,21 @@ def main():
     m_in = 1
     if int_input == 0:
         print(str(int_input))
-    if int_input < 0:
-        int_in = -1 * int_input
-        while int_in > 0:
-            rem_i = int_in % 10
-            m_in = m_in * rem_i
-            int_in = int_in // 10
-        m_in = m_in * -1
-        print(str(m_in))
-    else:
-        while int_input > 0:
-            rem_i = int_input % 10
-            m_in = m_in * rem_i
-            int_input = int_input // 10
-        print(str(m_in))
+    else:    
+        if int_input < 0:
+            int_in = -1 * int_input
+            while int_in > 0:
+                rem_i = int_in % 10
+                m_in = m_in * rem_i
+                int_in = int_in // 10
+            m_in = m_in * -1
+            print(str(m_in))
+        else:
+            while int_input > 0:
+                rem_i = int_input % 10
+                m_in = m_in * rem_i
+                int_input = int_input // 10
+                print(str(m_in))
 
 if __name__ == "__main__":
     main()
