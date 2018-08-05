71f160caa73d5472e88d2975e4e2f4130371074e
jgkr95 <jgkr95@msitprogram.net>
2018-08-05 18:33:20 +0530

    " -> 0 of 5 passed. pylint: 10/10 "

--- Practice/M4/p3/longest_substring.py
+++ Practice/M4/p3/longest_substring.py
@@ -1,5 +1,6 @@
 '''Assume s is a string of lower case characters.
-Write a program that prints the longest substring of s in which the letters occur in alphabetical order.
+Write a program that prints the longest
+substring of s in which the letters occur in alphabetical order
 For example, if s = 'azcbobobegghakl', then your program should print
 Longest substring in alphabetical order is: beggh
 In the case of ties, print the first substring.
@@ -7,19 +8,23 @@ For example, if s = 'abcbcd', then your program should print
 Longest substring in alphabetical order is: abc'''
 def main():
     '''This is main method '''
-    s = input()
-    max_str=""
-    max_len=0
-    for i in range(len(s)): 
-        if(i+1 < len(s)): 
-            if(s[i]<s[i+1]) : 
-                temp_len=1 
-                j=i+1       
-                while j+1 <len(s) and s[j]<=s[j+1]: 
-                        temp_len=temp_len+1
+    s_s = input()
+    print(s_s)
+    max_str = ""
+    max_len = 0
+    length_s = len(s_s)
+    for i in range(length_s):
+        if i+1 < length_s:
+            if s_s[i] < s_s[i+1]:
+                temp_len = 1
+                j = i+1
+                while j+1 < length_s and s_s[j] <= s_s[j+1]:
+                    temp_len = temp_len+1
+                    j += 1
 
-                if(temp_len > max_len): 
-                        max_str = s[i:j+1]
+                if temp_len > max_len:
+                    max_str = s_s[i:j+1]
+                    max_len = temp_len
     print(max_str)
 if __name__ == "__main__":
     main()
