b6618db71f045c142c09ceac703d4f735be84552
jgkr95 <jgkr95@msitprogram.net>
2018-08-07 14:59:58 +0530

    Gcd using iteration

---
+++ Practice/M8/GCD using Iteration/gcd_iter.py
@@ -0,0 +1,26 @@
+# Exercise: GCDIter
+# Write a Python function, gcdIter(a, b), that takes in two numbers and returns the GCD(a,b) of given a and b.
+
+# This function takes in two numbers and returns one number.
+
+
+def gcdIter(a, b):
+    '''
+    a, b: positive integers
+    
+    returns: a positive integer, the greatest common divisor of a & b.
+    '''
+    # Your code here
+    m_m = 1
+    for i in range(1,max(a,b)+1):
+    	if a%i == 0 and b%i == 0:
+    		if i > m_m:
+    			m_m = i
+    return m_m
+def main():
+    data = input()
+    data = data.split()
+    print(gcdIter(int(data[0]),int(data[1]))) 
+
+if __name__== "__main__":
+    main()
