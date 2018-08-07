4ae1df771b0843679e1879d76149ecb415dba4be
jgkr95 <jgkr95@msitprogram.net>
2018-08-07 14:38:59 +0530

    " -> 9 of 10 passed. pylint: 10/10 "

--- Practice/M8/p2/assignment2.py
+++ Practice/M8/p2/assignment2.py
@@ -14,7 +14,7 @@ def sumofdigits(n_n):
 def main():
     '''s'''
     import sys
-    sys.setrecursionlimit(45500)
+    sys.setrecursionlimit(415500)
     a_a = input()
     print(sumofdigits(int(a_a)))
 if __name__ == "__main__":
